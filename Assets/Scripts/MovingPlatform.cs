01ce4520d106d21dc7946c6834f35d50e8e74cc4
dmbeal <dbeal94@gmail.com>
2021-09-15 16:36:59 -0700

    Added Moving platform script + enabled platforms in level 4

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -6,15 +6,88 @@ public class MovingPlatform : MonoBehaviour
 {
     public GameObject Player;
 
+    public Vector3[] points;
+    public int point_number = 0;
+    public Vector3 current_target;
+
+    public float tolerance;
+    public float speed;
+    public float delay_time;
+
+    private float delay_start;
+
+    public bool automatic;
+    public bool isReversable;
+
+    void Start()
+    {
+        if(points.Length > 0)
+        {
+            current_target = points[0];
+        }
+        tolerance = speed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        if(this.transform.localPosition != current_target)
+        {
+            MovePlatform();
+        }
+        else
+        {
+            UpdateTarget();
+        }
+
+    }
+
+    void MovePlatform()
+    {
+        Vector3 heading = current_target - this.transform.localPosition;
+        this.transform.localPosition += (heading / heading.magnitude) * (speed * Time.deltaTime);
+        if(heading.magnitude < tolerance) 
+        {
+            this.transform.localPosition = current_target;
+            delay_start = Time.time;
+        }
+
+    }
+
+    void UpdateTarget()
+    {
+        if(automatic)
+        {
+            if(Time.time - delay_start > delay_time)
+            {
+                NextPlatform();
+            }
+        }
+    }
+
+    public void NextPlatform()
+    {
+        point_number ++;
+        if(point_number >= points.Length)
+        {
+            if (isReversable)
+            {
+               System.Array.Reverse(points);
+
+            }
+            point_number = 0;
+        }
+        current_target = points[point_number];
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == Player)
-            Player.transform.parent = transform;
+        other.transform.parent = transform;
+
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == Player)
-            Player.transform.parent = null;
+        other.transform.parent = null;
     }
 }
