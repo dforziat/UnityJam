0b3e6a46b856af9dc1b636a4fd1cbf097aed574b
dforziat <dforziat@gmail.com>
2021-10-03 18:24:28 +0900

    More work on the boss cutscene.

---
+++ Assets/Scripts/BossCutsceneManagerScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossCutsceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Canvas playerCanvas;
+    public GameObject boss;
+    public DoorProximityScript entranceDoor;
+    public AudioSource musicManager;
+    public Text bossName;
+
+    void Start()
+    {
+        boss.SetActive(false);
+        playerCanvas.enabled = false;
+        bossName.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartFight()
+    {
+        playerCanvas.enabled = true;
+        entranceDoor.isUnlocked = false;
+    }
+
+    public void ActivateBoss()
+    {
+        boss.SetActive(true);
+        bossName.enabled = true;
+        bossName.CrossFadeAlpha(0, 9, false);
+        musicManager.Play();
+    }
+}
