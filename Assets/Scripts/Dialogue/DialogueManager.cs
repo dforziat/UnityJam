e15355a427b0ff6394e984752ab90eae245670c6
dmbeal <dbeal94@gmail.com>
2022-02-25 19:41:38 -0800

    Dialogue System Working!

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public GameObject dialogueBox;
+
+    private Queue<string> sentences;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueBox.SetActive(false);
+        sentences = new Queue<string>();
+    }
+
+    private void Update()
+    {
+        NextLine();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentences();
+    }
+
+    public void DisplayNextSentences()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(.04f);
+            //yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
+
+    public void NextLine()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DisplayNextSentences();
+        }
+
+    }
+}
