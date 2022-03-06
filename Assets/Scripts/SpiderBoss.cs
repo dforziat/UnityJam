1c515490b3ebb2e3fd35b7ca90141c97667c2d66
dforziat <dforziat@gmail.com>
2022-03-06 17:52:41 +0900

    finished missile volley attack

--- Assets/Scripts/SpiderBoss.cs
+++ Assets/Scripts/SpiderBoss.cs
@@ -7,14 +7,21 @@ public class SpiderBoss : BossScript
 {
 
     private float walkSpeed = 1f;
+    private float spinSpeed = 3f;
     private int maxHealth = 100;
     private bool isDead = false;
+    private bool fireLeftMissile = true;
  
 
     public Transform head;
+    public Transform missileLauncher;
+    public GameObject missile;
+
+    public AudioClip missileLaunchClip;
 
     private Animator animator;
     private NavMeshAgent navMeshAgent;
+    private AudioSource audioSource;
     private Transform playerTransform;
     // Start is called before the first frame update
     void Start()
@@ -23,6 +30,9 @@ public class SpiderBoss : BossScript
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        secondaryAudioSource = GetComponent<AudioSource>();
+        animator.SetTrigger("missile");
     }
 
     // Update is called once per frame
@@ -42,11 +52,43 @@ public class SpiderBoss : BossScript
         }
         health -= damage;
 
-        //playDamagedClip();
+        playDamagedClip();
     }
 
     private void lookAtPlayer()
     {
         head.LookAt(playerTransform.position);
+    }
+
+    private void fireMissile()//Called from animation event
+    {
+        audioSource.PlayOneShot(missileLaunchClip);
+        Instantiate(missile, new Vector3(missileLauncher.transform.position.x, missileLauncher.transform.position.y, missileLauncher.transform.position.z), transform.rotation);
+    }
+
+    public void stopWalking()
+    {
+        navMeshAgent.speed = 0;
+    }
+
+    public void resumeWalking()
+    {
+        navMeshAgent.speed = walkSpeed;
+    }
+    private void playDamagedClip()
+    {
+        int randNum = Random.Range(1, 4);
+        switch (randNum)
+        {
+            case 1:
+                secondaryAudioSource.PlayOneShot(damagedClip);
+                break;
+            case 2:
+                secondaryAudioSource.PlayOneShot(damagedClip2);
+                break;
+            case 3:
+                secondaryAudioSource.PlayOneShot(damagedClip3);
+                break;
+        }
     }
 }
