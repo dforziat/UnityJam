b2b2cb6863010129db3545c1a45363f9415ddb10
dmbeal <dbeal94@gmail.com>
2022-03-14 19:02:34 -0700

    Starting Cutscene lvl 1

---
+++ Assets/Scripts/Cutscenes/BossCutsceneManagerScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossCutsceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject GUI;
+    public GameObject boss;
+    public DoorProximityScript entranceDoor;
+    public AudioSource musicManager;
+    public Text bossName;
+    public Text bossSubName;
+    public Image healthBar;
+
+
+    void Start()
+    {
+        boss.SetActive(false);
+        GUI.SetActive(false);
+        bossName.enabled = false;
+        bossSubName.enabled = false;
+        healthBar.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartFight()
+    {
+        GUI.SetActive(true);
+        entranceDoor.isUnlocked = false;
+        boss.GetComponent<Animator>().SetTrigger("enterRoom");
+    }
+
+    public void ActivateBoss()
+    {
+        boss.SetActive(true);
+        boss.GetComponent<LineRenderer>().enabled = false;
+        bossName.enabled = true;
+        bossName.CrossFadeAlpha(0, 8, false);
+        bossSubName.enabled = true;
+        bossSubName.CrossFadeAlpha(0, 8, false);
+        musicManager.Play();
+
+        healthBar.enabled = true;
+        healthBar.GetComponent<Animator>().SetTrigger("healthBarFill");
+    }
+}
---
+++ Assets/Scripts/Cutscenes/Level1CutsceneManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Level1CutsceneManager : MonoBehaviour
+{
+ 
+    public GameObject GUI;
+    public GameObject Dialogue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player starts with no HUD, timeline locks them in place
+        GUI.SetActive(false);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartLevel()
+    {
+        //enables hud, scene ends
+        GUI.SetActive(true);
+
+    }
+}
