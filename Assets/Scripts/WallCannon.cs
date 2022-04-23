25542bcc171b64869df9f23d9c29ad28a2a23493
dforziat <dforziat@gmail.com>
2022-04-23 19:21:30 +0900

    First pass on 2D wall cannon

---
+++ Assets/Scripts/WallCannon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCannon : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float fireRate = 120f;
+    public float timer;
+    public float projectileSpeed = 3f;
+    public GameObject projectile;
+
+    public AudioClip cannonShotClip;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        timer = fireRate;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            shootCannon();
+            timer = fireRate;
+        }
+
+    }
+
+    public void shootCannon()
+    {
+        audioSource.PlayOneShot(cannonShotClip);
+        GameObject cannonShot = Instantiate(projectile, transform.position, transform.rotation);
+        cannonShot.GetComponent<Rigidbody>().velocity = transform.forward * projectileSpeed;
+    }
+
+
+}
