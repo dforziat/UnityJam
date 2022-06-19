13c2583626ce4d1b3e981ce7e1f725d4d85774fd
dforziat <dforziat@gmail.com>
2022-06-19 12:00:08 +0900

    Boss3 Shoot state done

--- Assets/Boss3AnimationEvents.cs
+++ Assets/Boss3AnimationEvents.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Boss3AnimationEvents : MonoBehaviour
 {
     // Start is called before the first frame update
+
     public GameObject rifle;
     //Rifle stats
     Vector3 rifleRunPosition = new Vector3(1.2f, 0.56f, 0.67f);
@@ -13,13 +14,28 @@ public class Boss3AnimationEvents : MonoBehaviour
     Vector3 rifleShootPosition = new Vector3(0.589999974f, 0.637000024f, 1.528f);
     Vector3 rifleShootRotation = new Vector3(293.405579f, 58.6852112f, 343.351044f);
 
+    public ParticleSystem muzzleFlash;
+
+    public Transform muzzle;
+
+    public GameObject rifleBullet;
+    private float rifleBulletSpeed = 10f;
+
 
     public GameObject saber;
+
+    private AudioSource audioSource;
+    [Header("Audio")]
+    public AudioClip rifleShootClip;
+
+    private Transform target;
 
 
     void Start()
     {
         saber.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -40,18 +56,33 @@ public class Boss3AnimationEvents : MonoBehaviour
         rifle.SetActive(false);
     }
 
-    private void enableRifle()
+    public void enableRifle()
     {
         rifle.SetActive(true);
     }
 
-    private void enableSaber()
+    public void enableSaber()
     {
         saber.SetActive(true);
     }
 
-    private void disableSaber()
+    public void disableSaber()
     {
         saber.SetActive(false);
+    }
+
+    public void rifleShoot()
+    {
+        muzzleFlash.Play();
+        audioSource.PlayOneShot(rifleShootClip);
+        shootRifleBullet();
+    }
+
+    private void shootRifleBullet()
+    {
+        GameObject rifleBullet = Instantiate(this.rifleBullet);
+        rifleBullet.transform.position = muzzle.position;
+        var dir = (target.position - rifleBullet.transform.position).normalized;
+        rifleBullet.GetComponent<Rigidbody>().velocity = dir * rifleBulletSpeed;
     }
 }
