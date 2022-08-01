7756e3f037bf4a4e1f20ac2d1adfb61e608d686a
dmbeal <dbeal94@gmail.com>
2022-07-31 19:16:08 -0700

    Bugs + Dynamic Main Menu

---
+++ Assets/Scripts/Menus/DynamicMainMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicMainMenu : MonoBehaviour
+{
+    public GameObject Act1;
+    public GameObject Act2;
+    public GameObject Act3;
+
+    // if cur level <=6 = act 1 
+    // if cur level >6 && <=12 = act 2 
+    // if cur level >12 = act 3
+    //else act 1
+
+    private void Awake()
+    {
+
+        Debug.Log("CUR LEVEL= "+PlayerPrefs.GetInt(PlayerPrefsConstants.CUR_LVL));
+        if(PlayerPrefs.GetInt(PlayerPrefsConstants.CUR_LVL) <=6)
+        {
+            Act1.SetActive(true);
+            Act2.SetActive(false);
+            Act3.SetActive(false);
+            return;
+        }
+        if (PlayerPrefs.GetInt(PlayerPrefsConstants.CUR_LVL) > 6 && PlayerPrefs.GetInt(PlayerPrefsConstants.CUR_LVL) <= 12)
+        {
+            Act1.SetActive(false);
+            Act2.SetActive(true);
+            Act3.SetActive(false);
+            return;
+        }
+        if (PlayerPrefs.GetInt(PlayerPrefsConstants.CUR_LVL) > 12)
+        {
+            Act1.SetActive(false);
+            Act2.SetActive(false);
+            Act3.SetActive(true);
+            return;
+        }
+        else
+        {
+            Act1.SetActive(true);
+            Act2.SetActive(false);
+            Act3.SetActive(false);
+        }
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //test
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
