7608d48628082f0fa1d7acf502667d22604d72e3
dforziat <dforziat@gmail.com>
2022-08-04 15:53:44 +0900

    small bug fixes

---
+++ Assets/Scripts/SaveData/SaveData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public int currentLevel;
+    public int highestLevel;
+    public int curWeapon;
+    public bool shotgun;
+    public bool grapplegun;
+    public bool richochetgun;
+    public bool machinegun;
+    public bool spear;
+
+
+    public int resolutionWidth;
+    public int resolutionHeight;
+
+    public float masterVolume;
+    public float sfxVolume;
+    public float musicVolume;
+
+    public float mouseSens;
+
+    public float[] bestTime;
+    public float challengeBestTime;
+
+   public static SaveData Instance;
+
+
+}
---
+++ Assets/Scripts/SaveData/SaveDataInitializer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveDataInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool hasLoaded = false;
+    public static SaveDataInitializer instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            GameObject go = new GameObject("SaveDataInitializer");
+            go.AddComponent<SaveDataInitializer>();
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        SaveSystem.load();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveSystem.save();
+    }
+}
