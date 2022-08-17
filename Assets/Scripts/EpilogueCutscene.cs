9f89abef44a0aa4192b750f035eff173a9ca93e2
dmbeal <dbeal94@gmail.com>
2022-08-16 20:33:15 -0700

    fuck timelines

---
+++ Assets/Scripts/EpilogueCutscene.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+
+public class EpilogueCutscene : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public GameObject GUI;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        timeline = GetComponent<PlayableDirector>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+            GUI.SetActive(false);
+
+
+        }
+
+    }
+}
