829e9fa039d9fb87d322105ef5f4c887573fdb6e
dmbeal <dbeal94@gmail.com>
2022-08-21 19:55:12 -0700

    finished Dev Times + Bug Fixes

--- Assets/Scripts/DevTimes.cs
+++ Assets/Scripts/DevTimes.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public static class DevTimes 
 {
     //The third element in the array is Level1
-    public static float[] devTimesList = new float[] { 0.0f, 0.0f, 49.43f, 43.66f, 36.44f, 57.91f, 35.46f, 41.62f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+    public static float[] devTimesList = new float[] { 0.0f, 0.0f, 49.43f, 43.66f, 36.44f, 57.91f, 35.46f, 41.62f, 76.34f, 49.40f, 53.56f, 102.02f, 71.41f, 61.11f, 50.70f, 38.70f, 52.02f, 40.19f, 76.0f, 95.74f };
  
 }
